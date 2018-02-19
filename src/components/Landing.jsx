ed2230d0fb08d2f9a5981a4982f2113b50db552b
FullFaceRub <danielhbury@gmail.com>
2018-02-19 16:13:03 -0700

    added component folder and files

---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Landing extends Component {
+    render() {
+        return (
+            <div className="landing">
+                <div className="login">
+                    <img src="" alt="logo" className="landingLogo"/>
+                    <h1>Please Login or Signup</h1>
+                    <a href="">Login</a>
+                </div>
+            </div>
+        )
+    }
+}
