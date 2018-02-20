bd994371182190d831d6206ceb21ae67556f8212
FullFaceRub <danielhbury@gmail.com>
2018-02-20 14:28:40 -0700

    started on account page styling and skelly

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import {connect } from 'react-redux';
+import { connect } from 'react-redux';
+import Header from './Header';
+import { getUserInfo } from '../ducks/reducer';
 
 
-class Account extends Component{
+class Account extends Component {
 
-    componentDidMount(){
+    componentDidMount() {
         this.props.getUserInfo()
+        console.log(this.props.user);
     }
 
 
-    render(){
-        return(
+    render() {
+        return (
             <div>
-                this is the Account page, succa
+                <Header />
+                <div className="accountAvatar">
+                    <img className="accountImage" src="" alt="You"/>
+                    <div>
+                        <div className="accountName"></div>
+                        <div className="accountUserName"></div>
+                    </div>
+                </div>
+                <div className="accountLinks">
+                    <div>Update Acount Info</div>
+                    <div>Order History</div>
+                    <div>Draft Books</div>
+                </div>
             </div>
         )
     }
 }
-function mapStateToProps( state ){
+function mapStateToProps(state) {
     return {
         user: state.user
     }
 
 }
-export default connect( mapStateToProps, { getUserInfo } ) (Account );
+export default connect(mapStateToProps, { getUserInfo })(Account);
