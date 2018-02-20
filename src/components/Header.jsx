9a285d3696c9ff5c2b12b95b6831d9cd9a9ffd2b
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2018-02-19 17:06:18 -0700

    A little header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,10 @@ export default class Header extends Component{
     render(){
         return(
             <div>
-                <Link to="/home"><div>Home</div></Link> 
-                <Link to="/newbook"><div>Add New Book</div></Link> 
-                <Link to="/account"><div>Account</div></Link> 
-                <Link to="/drafts"><div>Drafts</div></Link> 
+                <Link to="/home" style={{textDecoration: 'none'}}><div>Home</div></Link> 
+                <Link to="/newbook" style={{textDecoration: 'none'}}><div>Add New Book</div></Link> 
+                <Link to="/account" style={{textDecoration: 'none'}}><div>Account</div></Link> 
+                <Link to="/drafts" style={{textDecoration: 'none'}}><div>Drafts</div></Link> 
             </div>
         )
     }
