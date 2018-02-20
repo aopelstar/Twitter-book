87c59c66e5510b3a8fa47b2f41f67d4c0699e641
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2018-02-20 14:03:46 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React, { Component } from 'react';
 import './reset.css';
 import './App.css';
-import Header from './components/Header';
 import Routes from './routes';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header />
         {Routes}
       </div>
     );
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -6,13 +6,8 @@ class Landing extends Component {
             <div className="landing">
                 <div className="login">
                     <img src="" alt="logo" className="landingLogo" />
-<<<<<<< HEAD
-                    <h1>Please Login or Signup</h1>
-                    <a href={process.env.REACT_APP_LOGIN}><button>Login</button></a>
-=======
                     <h1>Welcome to TwitterBook</h1>
                     <a href={process.env.REACT_APP_LOGIN}><button>Login / Signup</button></a>
->>>>>>> master
                 </div>
             </div>
         )
