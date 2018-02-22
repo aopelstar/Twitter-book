3f4ca5a9c790fde0cfc79b427a47397469402d88
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-02-22 14:03:20 -0700

    set up check for sessions

--- server/controller/twitter_controller.js
+++ server/controller/twitter_controller.js
@@ -2,15 +2,5 @@ let axios = require('axios');
 
 
 module.exports ={
-    getTweets: (req, res, next) => {
-        const promise = axios.get(`https://api.twitter.com/1.1/statuses/user_timeline.json?user_id=${req.user.id}`)
-        promise.then( tweets => {
-            res.status(200).send(tweets)
-        })
-
-    },
-
-    searchTweets: (req, res, next) => {
-
-    }
+    
 }
