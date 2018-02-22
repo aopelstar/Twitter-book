baa854680ff15c3be15f2b9331fb86c48a2d1f19
FullFaceRub <danielhbury@gmail.com>
2018-02-22 13:53:15 -0700

    added link to top profile pic, we are receiving data on the account page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { getUserInfo } from '../ducks/reducer'
+import axios from 'axios';
 
 class Header extends Component {
     constructor() {
@@ -15,8 +16,10 @@ class Header extends Component {
     async componentDidMount() {
         await this.props.getUserInfo()
         const user = this.props.user
-        this.setState({
-            user: user.data
+        axios.get("/api/twitter").then(res => {
+            this.setState({
+                user: res.data.data[0].user
+            })
         })
     }
     hamMenuSlide() {
@@ -25,11 +28,14 @@ class Header extends Component {
         })
     }
     render() {
+
+        let image = this.state.user.profile_image_url?this.state.user.profile_image_url.replace('normal', '400x400'):null
+
         return (
             <div>
                 <div className="header">
-                    <img src="" alt="logo" />
-                    <img src={this.state.user.user_image} className="headerProfileImg" alt="profile pic" />
+                    <Link to="/home"><img src="" alt="logo" /></Link>
+                    <Link to="/account"><img src={image} className="headerProfileImg" alt="profile pic" /></Link>
                     <div className="hamMenuContainer">
                         <div className={this.state.HamAnimation ? "hamMenu" : "hamMenu HamSlide"} onClick={() => this.hamMenuSlide()}>
                             <div className="hamLine one"></div>
