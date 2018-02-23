ce92617d5c826db35a082269bd263bd43a002266
Justin DeMarco <justindemarco@Justins-MacBook-Pro.local>
2018-02-23 10:12:19 -0700

    commit

--- src/components/NewBook.jsx
+++ src/components/NewBook.jsx
@@ -2,10 +2,20 @@ import React, { Component } from 'react';
 
 
 export default class NewBook extends Component{
+    constructor(){
+        super();
+        this.state = {
+            position: 1
+        }
+    }
     render(){
         return(
             <div>
-                this is the new book page
+                {this.state.position === 1 ? null : null}
+                {this.state.position === 2 ? null : null}
+                {this.state.position === 3 ? null : null}
+                {this.state.position === 4 ? null : null}
+                {this.state.position === 5 ? null : null}
             </div>
         )
     }
