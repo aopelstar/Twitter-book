235b686e1f9e86e3aeb3e3756809206f1aab2054
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-02-26 12:15:55 -0700

    adding stipe to step 6.  Dan the mentor is a dream boat.  *heart eyes emoji*

--- src/components/steps/Step6.jsx
+++ src/components/steps/Step6.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+export default class Step6 extends Component {
+    constructor(){
+        super(props)
+    }
 
 
-export default function Step6() {
-    return (
-        <div>
+    onToken = (token) =>{
+        fetch('/save-stripe-token', {
+            method: 'POST',
+            body: JSON.stringify(toke),
+        }).then(response => {
+            response.json().then(data => {
+                alert(`we are in business, ${data.email}`);
+            })
+        })
+    }
 
-        </div>
-    )
+    render(){
+        return(
+            <div>
+                <StripeCheckout
+                token={this.onToken}
+                stripeKey="my_PUBLISHABLE_stripekey"
+                />
+            </div>
+        )
+    }
 }
 
