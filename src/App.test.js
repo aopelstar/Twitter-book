97f429c6f1c97b21eddc58f164ebb8c19c86ad45
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-02-27 10:56:47 -0700

    ade my five functional tests

--- src/App.test.js
+++ src/App.test.js
@@ -24,3 +24,36 @@ describe('removing the retweet tag from the text', () => {
     expect(typeof text).toBe('string')
   })
 })
+
+
+//Andrew's test
+describe('just some tests for my functions', ()=>{
+
+  test('checking to see if a user exists', ()=> {
+    let user = fns.userExist();
+    expect(user).toBe("Juan Castro")
+  })
+
+  test('checking that user is returned as text', ()=> {
+    let user = fns.userExist();
+    expect(typeof user).toBe('string');
+  })
+
+  test('check that parameters are in an array', () => {
+    let params = fns.isAnArray();
+    expect(Array.isArray(params)).toBe(true)
+  })
+
+  test('check the array is 6 elements', () => {
+    let params = fns.isAnArray();
+    expect(params.length).toEqual(6)
+  })
+
+  test('that the image size is correct', () => {
+    let image = fns.imageSize();
+    expect(image).toBe("profilepic+400x400")
+  })
+
+})
+
+//Dan's test
