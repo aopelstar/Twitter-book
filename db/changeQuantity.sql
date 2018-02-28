33812accfb6f7b00faf28cb00eed30b85edaf7a2
FullFaceRub <danielhbury@gmail.com>
2018-02-28 10:21:41 -0700

    added function for changing quantity of cart items in the twitter controller

--- db/changeQuantity.sql
+++ db/changeQuantity.sql
@@ -1,3 +1,5 @@
 update cart
 set quantity = $3
 where user_id = $1 and book_id = $2
+
+returning *
--- db/create_orderline.sql
+++ db/create_orderline.sql
@@ -1,3 +1,5 @@
 insert into orderlines
 (order_id, book_id, quantity, book_price)
 values ($1,$2,$3,(select book_price from books where book_id = $2))
+
+returning *
