56a1a64cb0f9282ce8909e16c15d1d12c4e98921
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-03-11 23:59:26 -0600

    account redirect

--- server/index.js
+++ server/index.js
@@ -80,7 +80,7 @@ passport.deserializeUser((user, done) => {
 
 app.get('/auth', passport.authenticate('auth0'));
 app.get('/auth/callback', passport.authenticate('auth0', {
-    successRedirect: '/home',
+    successRedirect: '/account',
     failureRedirect: '/'
 }));
 
