bfc5d49de1bcd60be1128b67e678dc2d1c499097
Andrew Keele <hiimandyk@MacBook-Pro.local>
2018-03-12 09:38:25 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="mainBody">
-          {window.location.pathname === "/" ? null : <Header />}
+          {window.location.pathname === "#/" ? null : <Header />}
         {Routes}
       </div>
     );
